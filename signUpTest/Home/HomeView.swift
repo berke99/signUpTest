0c93e8ab63914259b969295ec4cef4ba08bfa081
Berke Kesgin <kesginberke9@gmail.com>
2024-09-09 15:51:21 +0300

    HomeView basic bugs fixed

--- signUpTest/Home/HomeView.swift
+++ signUpTest/Home/HomeView.swift
@@ -9,15 +9,21 @@ import SwiftUI
 import MapKit
 
 struct HomeView: View {
+
+    private let coordinate = CLLocationCoordinate2D(latitude: 41.0082, longitude: 28.9784)
+
     var body: some View {
-        NavigationView{
+        NavigationView {
             VStack {
-                Map()
-            
-        .navigationBarTitle("Ana Sayfa", displayMode: .large)
+                Map(coordinateRegion: .constant(MKCoordinateRegion(
+                    center: coordinate,
+                    span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+                )))
+                .edgesIgnoringSafeArea(.all)
+
+                
             }
         }
-
     }
 }
 
