c105e812a141aacecfa268b0f950dac302c89a41
Berke Kesgin <kesginberke9@gmail.com>
2024-09-09 13:55:29 +0300

    Initial Commit

---
+++ signUpTest/signUpTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  signUpTestApp.swift
+//  signUpTest
+//
+//  Created by Berke Kesgin on 9.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct signUpTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
